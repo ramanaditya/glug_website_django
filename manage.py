08845e62f930edf401ed27b56dd8da6f815658ad
Arsh Deep <arshdeepkashyap33@gmail.com>
2019-04-30 19:31:46 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         
     BASE_DIRS = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     dir_add = os.path.join(
-        BASE_DIRS, "glugmvit-web-firebase-adminsdk-fcfa3-d4143f72cc.json")
+        BASE_DIRS, "glug_website_django/glugmvit-web-firebase-adminsdk-fcfa3-d4143f72cc.json")
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = dir_add
     bq_client = Client()
     
