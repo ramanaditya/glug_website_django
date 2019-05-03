bec2b851a069fab8b3234ba245981715256272e6
Arsh Deep <arshdeepkashyap33@gmail.com>
2019-05-03 18:32:46 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,8 @@ from django.conf import settings
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glug.settings')
     try:
+        from django import db
+        db.connections.close_all()
         
         from django.core.management import execute_from_command_line
     except ImportError as exc:
